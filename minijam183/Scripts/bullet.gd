ecb0bc8b0f329f6e1bcd9cd9536f604a4ee349ac
SolennEstier <solenn.estier@gmail.com>
2025-04-25 20:49:18 +0200

    added bullets

--- minijam183/Scripts/bullet.gd
+++ minijam183/Scripts/bullet.gd
@@ -2,3 +2,7 @@ extends RigidBody2D
 
 var color: String
 var characteristics : Array
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- minijam183/Scripts/tower.gd
+++ minijam183/Scripts/tower.gd
@@ -2,11 +2,13 @@ extends StaticBody2D
 
 signal new_bullet(direction, speed, characteristics)
 
+var bullet_scene = load("res://Scenes/bullet.tscn")
+
 @export var number : int
 var color: int = 1
 var power: int = 1
-var range: int = 100
-var shot_speed: int = 10
+var range: int = 1000
+var shot_speed: int = 400
 var closestDistance: int
 
 func _on_tower_timer_timeout() -> void:
@@ -16,6 +18,7 @@ func find_closest_enemy():
 	var closestEnemy
 	closestDistance = range
 	var all_enemy = get_tree().get_nodes_in_group("enemy")
+	print(all_enemy)
 	for enemy in all_enemy:
 		var gun2enemy_distance = position.distance_to(enemy.position)
 		if gun2enemy_distance < closestDistance:
@@ -24,10 +27,28 @@ func find_closest_enemy():
 	return closestEnemy
 
 func fire():
+	print(bullet_scene)
 	# detect closest enemy
 	var closestEnemy = find_closest_enemy()
+	print(closestEnemy)
 	if closestEnemy != null :
 		# create new bullet, with speed and rotation and characteristics (?)
 		var direction = closestEnemy.position - self.position
-		new_bullet.emit(direction.normalize(), shot_speed, [])
+		print(direction)
+		new_bullet.emit(direction.normalized(), shot_speed, [])
+		var bullet = bullet_scene.instantiate()
+		# Choose a random location on Path2D.
+		#var mob_spawn_location = $MobPath/MobSpawnLocation
+		#mob_spawn_location.progress_ratio = randf()
+
+		# Set the mob's position to the random location.
+		#bullet.position = self.position
+
+		#Choose the velocity for the mob.
+		#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+		bullet.linear_velocity = direction.normalized()*shot_speed
+
+		# Spawn the mob by adding it to the Main scene.
+		add_child(bullet)
+		
 	pass
