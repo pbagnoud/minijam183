10e8c0bf907b9d0343999b79a95a334647150b63
VB <119983496+vibonnier@users.noreply.github.com>
2025-04-25 20:16:08 +0200

    queue free when ifinish path

--- minijam183/Scripts/path_2d.gd
+++ minijam183/Scripts/path_2d.gd
@@ -1,7 +1,7 @@
 extends Path2D
 
 var timer = 0 
-var spawntime = 2
+var spawntime = 1
 var follower = preload("res://Scenes/path_follow_2d.tscn")
 
 func _process(delta: float) -> void:
--- minijam183/Scripts/path_follow_2d.gd
+++ minijam183/Scripts/path_follow_2d.gd
@@ -1,6 +1,9 @@
 extends PathFollow2D
 
-@export var runspeed = 200
+@export var runspeed = 1000
 
 func _process(delta: float) -> void:
 		set_progress(progress + runspeed * delta)
+		
+		if progress_ratio == 1:
+			queue_free()
