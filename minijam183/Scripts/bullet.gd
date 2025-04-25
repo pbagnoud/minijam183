42aed2b4a4d201051919ddf0a2716c95ace379fa
SolennEstier <solenn.estier@gmail.com>
2025-04-25 20:05:12 +0200

    started fire coding

--- minijam183/Scripts/bullet.gd
+++ minijam183/Scripts/bullet.gd
@@ -1,3 +1,4 @@
 extends RigidBody2D
 
 var color: String
+var characteristics : Array
--- minijam183/Scripts/tower.gd
+++ minijam183/Scripts/tower.gd
@@ -1,11 +1,33 @@
 extends StaticBody2D
 
-@export var number : int
-var color: int
+signal new_bullet(direction, speed, characteristics)
 
+@export var number : int
+var color: int = 1
+var power: int = 1
+var range: int = 100
+var shot_speed: int = 10
+var closestDistance: int
 
 func _on_tower_timer_timeout() -> void:
 	fire()
 
+func find_closest_enemy():
+	var closestEnemy
+	closestDistance = range
+	var all_enemy = get_tree().get_nodes_in_group("enemy")
+	for enemy in all_enemy:
+		var gun2enemy_distance = position.distance_to(enemy.position)
+		if gun2enemy_distance < closestDistance:
+			closestDistance = gun2enemy_distance
+			closestEnemy = enemy
+	return closestEnemy
+
 func fire():
+	# detect closest enemy
+	var closestEnemy = find_closest_enemy()
+	if closestEnemy != null :
+		# create new bullet, with speed and rotation and characteristics (?)
+		var direction = closestEnemy.position - self.position
+		new_bullet.emit(direction.normalize(), shot_speed, [])
 	pass
