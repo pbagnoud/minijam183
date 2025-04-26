92cdb3a9ef22aa766a5e3b1db31f5fc9951a2fb8
charl-baum <charlottebaumgart61@gmail.com>
2025-04-26 15:52:57 +0200

    bullet lifespan

--- minijam183/Scripts/bullet.gd
+++ minijam183/Scripts/bullet.gd
@@ -2,8 +2,11 @@ extends RigidBody2D
 
 var color: int
 var characteristics : Array
-var power:int =1
+var power:int = 1
+
+#func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	#queue_free()
 
 
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+func _on_timer_timeout() -> void:
 	queue_free()
