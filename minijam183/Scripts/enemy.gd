96c27cf8edda0a041d70e6b934d21f11494cac0c
PB <119977776+pbagnoud@users.noreply.github.com>
2025-04-26 18:30:50 +0200

    show color

--- minijam183/Scripts/enemy.gd
+++ minijam183/Scripts/enemy.gd
@@ -6,7 +6,13 @@ signal hit
 
 var real_position: Vector2
 var pv: int = 2
-var color: int = 1
+var color: int = 1:
+	set(value):
+		color=value
+		change_color(value)
+
+func _ready() -> void:
+	change_color(color)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullet") :
@@ -15,6 +21,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		else:
 			pv -= body.power
 		body.hit_something()
+		if body.has_color_change:
+			change_color(body.color)
 	if pv <= 0:
 		get_parent().queue_free()
 	
