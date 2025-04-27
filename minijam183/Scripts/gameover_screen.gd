d3aa29a0ea449be0a1225ae78bbb6ed7241cb58c
charl-baum <charlottebaumgart61@gmail.com>
2025-04-27 20:06:14 +0200

    sound updates

--- minijam183/Scripts/gameover_screen.gd
+++ minijam183/Scripts/gameover_screen.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@onready var click_sound: AudioStreamPlayer2D = $Click_sound
 
 func _on_button_start_pressed() -> void:
+	click_sound.playing = true
 	get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
 
 
 func _on_button_quit_pressed() -> void:
+	click_sound.playing = true
 	get_tree().quit()
